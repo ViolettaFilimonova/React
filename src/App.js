5cc08c8d587adbc4490e048ab8db48eff76f7e98
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-06-08 17:25:42 +0300

    component reuse

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,11 +12,15 @@ function App() {
         {id: 2, title: 'Python', text: 'JPython - язык программирования'},
         {id: 3, title: 'C++', text: 'C++ - язык программирования'}
         ])
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Javascript 2', text: 'Javascript - язык программирования'},
+        {id: 2, title: 'Python 2', text: 'JPython - язык программирования'},
+        {id: 3, title: 'C++ 2', text: 'C++ - язык программирования'}
+    ])
     return (
         <div className="App">
-            {posts.map(post =>
-                <PostItem post={post} key={post.id}/>
-            )}
+            <PostList posts={posts} title={'Список постов 1'}/>
+            <PostList posts={posts} title={'Список постов 2'}/>
         </div>
     );
 }
